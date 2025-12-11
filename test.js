b85430395d9664a9906a252c99c1c71b41ab7ed1
arushkandare <arushkandare@gmail.com>
2025-12-11 10:28:05 +0530

    Updated test.js

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
 console.log("Starting tests ...");
-setTimeout(() => console.log("Waiting 3 seconds ..."), 3000);
+
+await new Promise(resolve => setTimeout(resolve, 3000));
+
+console.log("Waiting 3 seconds ...");
 console.log("Tests complete!");
